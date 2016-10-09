9e1669a1f216a8dcaa206ce4c791caa90d0b3f87
Tao <taogeng3@gmail.com>
2016-10-08 23:37:13 -0700

    Update Ship.java

--- TreasureHunt/Ship.java
+++ TreasureHunt/Ship.java
@@ -11,20 +11,24 @@ public class Ship extends Actor
     Island currentIsland;
     
     public Ship(Island island, String filename) {
-        currentIsland = island;
+        currentIsland = island;        
+        setLocation(currentIsland.getX(), currentIsland.getY());
         setImage(filename);
     }
     
     public void gotoA(){
         currentIsland = currentIsland.getA();
+        setLocation(currentIsland.getX(), currentIsland.getY());
     }
     
     public void gotoB(){
         currentIsland = currentIsland.getB();
+        setLocation(currentIsland.getX(), currentIsland.getY());
     }
     
     public void moveTo(Island island){
         currentIsland = island;
+        setLocation(currentIsland.getX(), currentIsland.getY());
     }
     
     public void act() 
